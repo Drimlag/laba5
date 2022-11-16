6a0e86da83b97ae36bfcb819ebcb7e738c2cced1
Drimlag <111630433+Drimlag@users.noreply.github.com>
2022-11-16 21:55:22 +0200

    Add files via upload

---
+++ labaf.py
@@ -0,0 +1,13 @@
+[[source]]
+url = "https://Drimlag.org/simple"
+verify_ssl = true
+name = "Drimlag"
+
+[packages]
+
+[dev-packages]
+pytest = "*"
+coverage = "*"
+
+[requires]
+python_version = "3.10"
